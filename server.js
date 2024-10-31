46ed49ac87ac00f9105eac72ff6ab7b1f4b095ea
Tevrrr <piskynov.vlad@gmail.com>
2024-10-31 18:59:38 +0300

    fix

--- server.js
+++ server.js
@@ -4,47 +4,46 @@ const { parse } = require('url');
 const { readFileSync } = require('fs');
 const next = require('next');
 
-const hostname = 'localhost'
-const port1 = 80
-const port2 = 443
-const app = next({  hostname, port:port2});
+const hostname = 'localhost';
+const port1 = 80;
+const port2 = 443;
+const app = next({ hostname, port: port2 });
 const handle = app.getRequestHandler();
 
 const httpsOptions = {
-  key: readFileSync('/etc/letsencrypt/live/auto-bus.net/privkey.pem'),
-  cert: readFileSync('/etc/letsencrypt/live/auto-bus.net/cert.pem')
+	key: readFileSync('/etc/letsencrypt/live/europa-bus.net/privkey.pem'),
+	cert: readFileSync('/etc/letsencrypt/live/europa-bus.net/cert.pem'),
 };
 
-
 app.prepare().then(() => {
-  createServer(httpsOptions, async (req, res) => {
-    try {
-      // Be sure to pass `true` as the second argument to `url.parse`.
-      // This tells it to parse the query portion of the URL.
-      const parsedUrl = parse(req.url, true)
-      const { pathname, query } = parsedUrl
- 
-      if (pathname === '/a') {
-        await app.render(req, res, '/a', query)
-      } else if (pathname === '/b') {
-        await app.render(req, res, '/b', query)
-      } else {
-        await handle(req, res, parsedUrl)
-      }
-    } catch (err) {
-      console.error('Error occurred handling', req.url, err)
-      res.statusCode = 500
-      res.end('internal server error')
-    }
-  })
-    .once('error', (err) => {
-      console.error(err)
-      process.exit(1)
-    })
-    .listen(port2, () => {
-      console.log(`> Ready on https://${hostname}:${port2}`)
-    })
-})
+	createServer(httpsOptions, async (req, res) => {
+		try {
+			// Be sure to pass `true` as the second argument to `url.parse`.
+			// This tells it to parse the query portion of the URL.
+			const parsedUrl = parse(req.url, true);
+			const { pathname, query } = parsedUrl;
+
+			if (pathname === '/a') {
+				await app.render(req, res, '/a', query);
+			} else if (pathname === '/b') {
+				await app.render(req, res, '/b', query);
+			} else {
+				await handle(req, res, parsedUrl);
+			}
+		} catch (err) {
+			console.error('Error occurred handling', req.url, err);
+			res.statusCode = 500;
+			res.end('internal server error');
+		}
+	})
+		.once('error', (err) => {
+			console.error(err);
+			process.exit(1);
+		})
+		.listen(port2, () => {
+			console.log(`> Ready on https://${hostname}:${port2}`);
+		});
+});
 
 // const server = http.createServer((req, res) => {
 // //     res.statusCode = 200;
@@ -61,8 +60,8 @@ app.prepare().then(() => {
 // });
 
 const server = http.createServer((req, res) => {
-  res.writeHead(301,{Location: `https://${req.headers.host}${req.url}`});
-  res.end();
+	res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
+	res.end();
 });
 
 server.listen(port1);
