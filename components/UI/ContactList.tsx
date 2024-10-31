c6e00daf071342e2593cdae9e4316f5cac81f141
Tevrrr <piskynov.vlad@gmail.com>
2024-11-01 00:07:52 +0300

    add phone

--- components/UI/ContactList.tsx
+++ components/UI/ContactList.tsx
@@ -9,22 +9,8 @@ interface ContactListProps {}
 const ContactList: NextPage<ContactListProps> = () => {
 	return (
 		<div className=' flex flex-col gap-10 justify-between  grow'>
-			<ContactCard phone='+380 99 258 37 13' />
-			{/* <ContactCard phone='+380 99 258 37 31' /> */}
-			<div className='flex flex-col sm:flex-row justify-between  p-8 gap-4 bg-accent text-accent-content rounded-2xl'>
-				<div className='flex flex-col-reverse sm:flex-row items-center justify-between'>
-					<a href={`tel:+79493796689`}>
-						<h4>+7 949 379 66 89</h4>
-					</a>
-				</div>
-				<div className='flex sm:w-40'>
-					<ContactLink
-						href={`https://t.me/+79493796689`}
-						icont={<FaTelegram />}
-						text='Telegram'
-					/>
-				</div>
-			</div>
+			<ContactCard phone='+380 95 047 53 07' />
+			<ContactCard phone='+380 95 861 93 76' />
 			<div className='grow'></div>
 		</div>
 	);
