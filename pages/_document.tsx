4c94fd69785f3b9e3c2c3ea654beada0198e94d8
Tevrrr <piskynov.vlad@gmail.com>
2024-11-01 16:26:16 +0300

    add google

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,15 +6,19 @@ export default function Document() {
 	return (
 		<Html lang='ru'>
 			<Head>
-				<noscript>
-					<div>
-						<img
-							src='https://mc.yandex.ru/watch/12345678'
-							style={{ position: 'absolute', left: '-9999px' }}
-							alt=''
-						/>
-					</div>
-				</noscript>
+				{/* Google Tag Manager  */}
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); (https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f))
+})(window,document,'script','dataLayer','GTM-NH9ZK22J');
+              `,
+					}}
+				/>
+				{/* End Google Tag Manager */}
 
 				<link rel='preconnect' href='https://fonts.googleapis.com' />
 				<link
@@ -36,6 +40,16 @@ export default function Document() {
 					content='Ежедневные пассажирские перевозки из ЛНР, ДНР и РФ в страны ЕС и Украину. Консьерж сервис в Варшаве. Транспортировка животных. Перегон автомобиля. Денежные переводы и обмен валют'></meta>
 			</Head>
 			<body>
+				<noscript>
+					<iframe
+						src='https://www.googletagmanager.com/ns.html?id=GTM-NH9ZK22J'
+						height='0'
+						width='0'
+						style={{
+							display: 'none',
+							visibility: 'hidden',
+						}}></iframe>
+				</noscript>
 				<Main />
 				<NextScript />
 			</body>
