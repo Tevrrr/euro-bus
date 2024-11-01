caec6cfd1bca482667c2730f375d6899e5803763
Tevrrr <piskynov.vlad@gmail.com>
2024-11-01 14:53:00 +0300

    fix....

--- components/UI/ContactCard.tsx
+++ components/UI/ContactCard.tsx
@@ -1,14 +1,13 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 import { FaViber, FaTelegram, FaWhatsapp } from 'react-icons/fa';
 import ContactLink from './ContactLink';
 
 interface ContactCardProps {
-    phone: string;
-};
+	phone: string;
+}
 
-const ContactCard: NextPage<ContactCardProps> = ({phone}) => {
-
-    return (
+const ContactCard: NextPage<ContactCardProps> = ({ phone }) => {
+	return (
 		<div className='flex flex-col p-8 gap-4 bg-accent text-accent-content rounded-2xl'>
 			<div className='flex flex-col-reverse sm:flex-row items-center justify-between'>
 				<h4>
@@ -16,15 +15,6 @@ const ContactCard: NextPage<ContactCardProps> = ({phone}) => {
 				</h4>
 			</div>
 			<div className='flex flex-col sm:flex-row gap-4'>
-				<ContactLink
-					href={`viber://chat?number=${phone
-						.split(' ')
-						.join('')
-						.split('+')
-						.join('')}`}
-					icont={<FaViber />}
-					text='Viber'
-				/>
 				<ContactLink
 					href={`https://t.me/${phone.split(' ').join('')}`}
 					icont={<FaTelegram />}
@@ -39,9 +29,18 @@ const ContactCard: NextPage<ContactCardProps> = ({phone}) => {
 					icont={<FaWhatsapp />}
 					text='WhatsApp'
 				/>
+				<ContactLink
+					href={`viber://chat?number=${phone
+						.split(' ')
+						.join('')
+						.split('+')
+						.join('')}`}
+					icont={<FaViber />}
+					text='Viber'
+				/>
 			</div>
 		</div>
 	);
-}
+};
 
 export default ContactCard;
